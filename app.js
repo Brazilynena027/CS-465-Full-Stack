3b7ed20b6f86d654cdff584e96254f3ac20c79fd
Brazilynena027 <danielle.sousa@snhu.edu>
2025-02-18 08:16:59 -0500

    Module 6 Completed Baseline

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -32,6 +33,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// enable CORS
+app.use("/api", (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+
+
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,11 +7,14 @@ const tripsController = require('../controllers/trips');
 // define route for our trips endpoint
 router 
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripList
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController. tripsAddTrip); // POST Method Adds a trip
 
+    //Get method routes tripsFind by Code - requires parameters
 router
-    .route('/trips/:tripsCode')
-    .get(tripsController.tripslistCode);    
+    .route('/trips/:tripCode')
+    .get(tripsController.tripslistCode)
+    .put(tripsController.tripsUpdateTrip);    
 
 
 module.exports = router;
